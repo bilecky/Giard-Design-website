abb972ae40dd188e0f7baba63ca5771c585d2445
bilecky <bilecky95@gmail.com>
2023-08-16 09:17:49 +0200

    code cleanup

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const mobileMenuButton = document.getElementById('mobile-menu-button')
 const mobileMenu = document.getElementById('mobile-menu')
 const closeButton = mobileMenu.querySelector('.close-button')
@@ -9,36 +8,31 @@ const expandBtn = document.getElementById('expand-btn')
 const gradient = document.getElementById('gradient')
 
 
-document.addEventListener('DOMContentLoaded',  () => {
-	mobileMenuButton.addEventListener('click',  () => {
-		mobileMenu.classList.add('translate-x-0')
-	})
 
-	closeButton.addEventListener('click',  () => {
-		mobileMenu.classList.add('-translate-x-full')
-		mobileMenu.classList.remove('translate-x-0')
-	})
 
-   expandBtn.addEventListener('click', () => {
-      gradient.style.display = 'none'
-   })
+mobileMenuButton.addEventListener('click', () => {
+	mobileMenu.classList.add('translate-x-0')
 })
 
+closeButton.addEventListener('click', () => {
+	mobileMenu.classList.add('-translate-x-full')
+	mobileMenu.classList.remove('translate-x-0')
+})
 
+expandBtn.addEventListener('click', () => {
+	gradient.style.display = 'none'
+})
 
 gridItems.forEach(item => {
-   item.addEventListener("click", () => {
-       const imageSrc = item.querySelector('img').src;
-       popupImage.src = imageSrc;
-       popup.style.display = "flex";
-   });
-});
+	item.addEventListener('click', () => {
+		const imageSrc = item.querySelector('img').src
+		popupImage.src = imageSrc
+		popup.style.display = 'flex'
+	})
+})
 
-popup.addEventListener("click", event => {
-   if (event.target === popup) {
-       popup.style.display = "none";
-   }
-});
-
-
-
+popup.addEventListener('click', event => {
+	if (event.target === popup) {
+		popup.style.display = 'none'
+	}
+})
